7ec282ddfcdeffeb01589fbb5fac713e5f0da93d
신창호 <vkdghckdh@gmail.com>
2022-11-21 16:15:37 +0900

    메인페이지 만들기

--- lostfinder/lostfinder/src/main/java/com/lostfinder/place/PlaceController.java
+++ lostfinder/lostfinder/src/main/java/com/lostfinder/place/PlaceController.java
@@ -1,9 +1,11 @@
 package com.lostfinder.place;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class PlaceController {
 
-
+    // 버스 장소
+    // 지하철 장소
 }
